8c6c5a785eaa090af897c342612ba81d6380180f
Sathvika Dharani Bhartu <serein.reverie.meraki@gmail.com>
2026-07-24 19:35:57 +0530

    Rearrange server.js for improved readability

--- TaskManager/src/server.js
+++ TaskManager/src/server.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Task Manager API listening on port ${PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.json({
     application: "Cloud-Native Task Manager API",
     status: "Running",
     version: "1.0.0",
   });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Task Manager API listening on port ${PORT}`);
 });
