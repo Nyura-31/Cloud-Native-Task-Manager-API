ab12aead6df480cfcc7c626f23552c2ddf9a66b0
Sathvika Dharani Bhartu <serein.reverie.meraki@gmail.com>
2026-07-24 19:29:57 +0530

    Implement health check endpoint
    Add health check endpoint to server

--- TaskManager/src/server.js
+++ TaskManager/src/server.js
@@ -7,3 +7,11 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Task Manager API listening on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    application: "Cloud-Native Task Manager API",
+    status: "Running",
+    version: "1.0.0",
+  });
+});
